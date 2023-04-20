7fc36abb56750b4f868c1fa37ff69c4ce38254b4
SHIVAM VASHIST <vashistshivam931@gmail.com>
2023-04-20 16:17:14 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Setup from './tutorial/12-memo-useMemo-useCallback/final'
+function App() {
+  return (
+    <div className='container'>
+      <Setup/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/tutorial/11-react-router/setup/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">
+            <h3>Home</h3>
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/people">People</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
